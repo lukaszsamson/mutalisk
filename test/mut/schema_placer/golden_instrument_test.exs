adfb4c41c7f2475a8c6612b0d209fdfc90a6324d
Lukasz Samson <lukaszsamson@gmail.com>
2026-05-03 13:12:45 +0200

    Add instrumented source goldens

---
+++ test/mut/schema_placer/golden_instrument_test.exs
@@ -0,0 +1,31 @@
+defmodule Mut.SchemaPlacer.GoldenInstrumentTest do
+  use ExUnit.Case, async: false
+
+  @moduledoc false
+  @moduletag :golden_instrument
+
+  @fixture_root Path.expand("test/fixtures/demo_app")
+  @golden_root Path.expand("test/golden/instrument")
+  @fixture_files ~w(arith.ex cmp.ex bool.ex)
+
+  test "fixture instrumented sources match golden files" do
+    plan = Mut.Orchestrator.plan(@fixture_root, Mut.FixtureOracleHelper.golden_oracle())
+
+    Enum.each(@fixture_files, fn file ->
+      relative = "lib/#{file}"
+      path = Path.join(@fixture_root, relative)
+      mutants = Enum.filter(plan.schema, &(&1.file == relative))
+
+      assert {:ok, actual} = Mut.SchemaPlacer.instrument_file(path, mutants)
+
+      golden = Path.join(@golden_root, "#{Path.rootname(file)}.ex.txt")
+
+      if System.get_env("MUT_REGOLD") == "1" do
+        File.mkdir_p!(@golden_root)
+        File.write!(golden, actual)
+      end
+
+      assert actual == File.read!(golden)
+    end)
+  end
+end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,4 +1,4 @@
 Code.require_file("support/mutator_test_support.ex", __DIR__)
 Code.require_file("support/fixture_oracle_helper.ex", __DIR__)
 
-ExUnit.start(exclude: [:integration, :e2e, :golden_oracle])
+ExUnit.start(exclude: [:integration, :e2e, :golden_oracle, :golden_instrument])
